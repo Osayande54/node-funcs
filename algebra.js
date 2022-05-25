6babe8ad0271247775f8d8a6905dac50bbbf6fb5
Osayande54 <102307167+Osayande54@users.noreply.github.com>
2022-05-25 19:42:58 +0100

    Add files via upload

--- algebra.js
+++ algebra.js
@@ -1,12 +1,17 @@
 function add(a, b) {
   return a + b;
 }
+
 function subtract(c, d) {
   return c - d;
 }
+
 function multiply(e, f) {
   return e * f;
 }
+
 function divide(g, h) {
   return g / h;
 }
+
+module.exports = { add, subtract, multiply, divide };
