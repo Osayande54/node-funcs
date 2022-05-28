c6ac52f888e40c0f5b95b52f981fe9f09bd40c3e
Osayande54 <102307167+Osayande54@users.noreply.github.com>
2022-05-28 17:24:18 +0100

    Add files via upload

--- loop.js
+++ loop.js
@@ -1,4 +1,15 @@
-function myLoop() {
-  for (var a = 0; a < 101; a++) console.log(a);
+function myloop() {
+  for (var a = 0; a < 101; a++) {
+    console.log(a);
+  }
+
 }
+console.log(myloop())
+function myStudy(){
+  let Bars= ['depot ','agbor','Estate']
+  for(var i = 0; i < Bars.length; i++)
+  console.log(Bars[i])
+}
+console.log(myStudy())
+
 
